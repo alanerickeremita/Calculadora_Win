b49aaa7adb07530906f2f1c886ec6222ac0a1222
alanerickeremita <alanerickeremita@gmail.com>
2021-02-24 22:27:37 -0300

    Adicionar arquivos de projeto.

---
+++ calculadoraForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Calculadora_Win
+{
+    partial class calculadoraForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.telaExibicao = new System.Windows.Forms.TextBox();
+      this.botao1 = new System.Windows.Forms.Button();
+      this.botao2 = new System.Windows.Forms.Button();
+      this.botao3 = new System.Windows.Forms.Button();
+      this.botao4 = new System.Windows.Forms.Button();
+      this.botao5 = new System.Windows.Forms.Button();
+      this.botao6 = new System.Windows.Forms.Button();
+      this.botao7 = new System.Windows.Forms.Button();
+      this.botao8 = new System.Windows.Forms.Button();
+      this.botao9 = new System.Windows.Forms.Button();
+      this.botao0 = new System.Windows.Forms.Button();
+      this.botaoSubtracao = new System.Windows.Forms.Button();
+      this.botaoSoma = new System.Windows.Forms.Button();
+      this.botaoMultiplicacao = new System.Windows.Forms.Button();
+      this.botaoDivisao = new System.Windows.Forms.Button();
+      this.botaoIgualdade = new System.Windows.Forms.Button();
+      this.botaoC = new System.Windows.Forms.Button();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // telaExibicao
+      // 
+      this.telaExibicao.Location = new System.Drawing.Point(12, 28);
+      this.telaExibicao.Name = "telaExibicao";
+      this.telaExibicao.Size = new System.Drawing.Size(298, 22);
+      this.telaExibicao.TabIndex = 0;
+      this.telaExibicao.Tag = "Resultado:";
+      // 
+      // botao1
+      // 
+      this.botao1.Location = new System.Drawing.Point(12, 74);
+      this.botao1.Name = "botao1";
+      this.botao1.Size = new System.Drawing.Size(75, 23);
+      this.botao1.TabIndex = 1;
+      this.botao1.Text = "1";
+      this.botao1.UseVisualStyleBackColor = true;
+      this.botao1.Click += new System.EventHandler(this.botao1_Click);
+      // 
+      // botao2
+      // 
+      this.botao2.Location = new System.Drawing.Point(93, 74);
+      this.botao2.Name = "botao2";
+      this.botao2.Size = new System.Drawing.Size(75, 23);
+      this.botao2.TabIndex = 2;
+      this.botao2.Text = "2";
+      this.botao2.UseVisualStyleBackColor = true;
+      this.botao2.Click += new System.EventHandler(this.botao2_Click);
+      // 
+      // botao3
+      // 
+      this.botao3.Location = new System.Drawing.Point(174, 74);
+      this.botao3.Name = "botao3";
+      this.botao3.Size = new System.Drawing.Size(75, 23);
+      this.botao3.TabIndex = 3;
+      this.botao3.Text = "3";
+      this.botao3.UseVisualStyleBackColor = true;
+      this.botao3.Click += new System.EventHandler(this.botao3_Click);
+      // 
+      // botao4
+      // 
+      this.botao4.Location = new System.Drawing.Point(12, 103);
+      this.botao4.Name = "botao4";
+      this.botao4.Size = new System.Drawing.Size(75, 23);
+      this.botao4.TabIndex = 4;
+      this.botao4.Text = "4";
+      this.botao4.UseVisualStyleBackColor = true;
+      this.botao4.Click += new System.EventHandler(this.botao4_Click);
+      // 
+      // botao5
+      // 
+      this.botao5.Location = new System.Drawing.Point(93, 103);
+      this.botao5.Name = "botao5";
+      this.botao5.Size = new System.Drawing.Size(75, 23);
+      this.botao5.TabIndex = 5;
+      this.botao5.Text = "5";
+      this.botao5.UseVisualStyleBackColor = true;
+      this.botao5.Click += new System.EventHandler(this.botao5_Click);
+      // 
+      // botao6
+      // 
+      this.botao6.Location = new System.Drawing.Point(174, 103);
+      this.botao6.Name = "botao6";
+      this.botao6.Size = new System.Drawing.Size(75, 23);
+      this.botao6.TabIndex = 6;
+      this.botao6.Text = "6";
+      this.botao6.UseVisualStyleBackColor = true;
+      this.botao6.Click += new System.EventHandler(this.botao6_Click);
+      // 
+      // botao7
+      // 
+      this.botao7.Location = new System.Drawing.Point(12, 132);
+      this.botao7.Name = "botao7";
+      this.botao7.Size = new System.Drawing.Size(75, 23);
+      this.botao7.TabIndex = 7;
+      this.botao7.Text = "7";
+      this.botao7.UseVisualStyleBackColor = true;
+      this.botao7.Click += new System.EventHandler(this.botao7_Click);
+      // 
+      // botao8
+      // 
+      this.botao8.Location = new System.Drawing.Point(93, 132);
+      this.botao8.Name = "botao8";
+      this.botao8.Size = new System.Drawing.Size(75, 23);
+      this.botao8.TabIndex = 8;
+      this.botao8.Text = "8";
+      this.botao8.UseVisualStyleBackColor = true;
+      this.botao8.Click += new System.EventHandler(this.botao8_Click);
+      // 
+      // botao9
+      // 
+      this.botao9.Location = new System.Drawing.Point(174, 132);
+      this.botao9.Name = "botao9";
+      this.botao9.Size = new System.Drawing.Size(75, 23);
+      this.botao9.TabIndex = 9;
+      this.botao9.Text = "9";
+      this.botao9.UseVisualStyleBackColor = true;
+      this.botao9.Click += new System.EventHandler(this.botao9_Click);
+      // 
+      // botao0
+      // 
+      this.botao0.Location = new System.Drawing.Point(93, 161);
+      this.botao0.Name = "botao0";
+      this.botao0.Size = new System.Drawing.Size(75, 23);
+      this.botao0.TabIndex = 10;
+      this.botao0.Text = "0";
+      this.botao0.UseVisualStyleBackColor = true;
+      this.botao0.Click += new System.EventHandler(this.botao0_Click);
+      // 
+      // botaoSubtracao
+      // 
+      this.botaoSubtracao.Location = new System.Drawing.Point(255, 74);
+      this.botaoSubtracao.Name = "botaoSubtracao";
+      this.botaoSubtracao.Size = new System.Drawing.Size(55, 23);
+      this.botaoSubtracao.TabIndex = 13;
+      this.botaoSubtracao.Text = "-";
+      this.botaoSubtracao.UseVisualStyleBackColor = true;
+      this.botaoSubtracao.Click += new System.EventHandler(this.botaoSubtracao_Click);
+      // 
+      // botaoSoma
+      // 
+      this.botaoSoma.Location = new System.Drawing.Point(255, 103);
+      this.botaoSoma.Name = "botaoSoma";
+      this.botaoSoma.Size = new System.Drawing.Size(55, 23);
+      this.botaoSoma.TabIndex = 14;
+      this.botaoSoma.Text = "+";
+      this.botaoSoma.UseVisualStyleBackColor = true;
+      this.botaoSoma.Click += new System.EventHandler(this.botaoSoma_Click);
+      // 
+      // botaoMultiplicacao
+      // 
+      this.botaoMultiplicacao.Location = new System.Drawing.Point(255, 132);
+      this.botaoMultiplicacao.Name = "botaoMultiplicacao";
+      this.botaoMultiplicacao.Size = new System.Drawing.Size(55, 23);
+      this.botaoMultiplicacao.TabIndex = 15;
+      this.botaoMultiplicacao.Text = "*";
+      this.botaoMultiplicacao.UseVisualStyleBackColor = true;
+      this.botaoMultiplicacao.Click += new System.EventHandler(this.botaoMultiplicacao_Click);
+      // 
+      // botaoDivisao
+      // 
+      this.botaoDivisao.Location = new System.Drawing.Point(255, 161);
+      this.botaoDivisao.Name = "botaoDivisao";
+      this.botaoDivisao.Size = new System.Drawing.Size(55, 23);
+      this.botaoDivisao.TabIndex = 16;
+      this.botaoDivisao.Text = "/";
+      this.botaoDivisao.UseVisualStyleBackColor = true;
+      this.botaoDivisao.Click += new System.EventHandler(this.botaoDivisao_Click);
+      // 
+      // botaoIgualdade
+      // 
+      this.botaoIgualdade.Location = new System.Drawing.Point(12, 190);
+      this.botaoIgualdade.Name = "botaoIgualdade";
+      this.botaoIgualdade.Size = new System.Drawing.Size(298, 23);
+      this.botaoIgualdade.TabIndex = 17;
+      this.botaoIgualdade.Text = "=";
+      this.botaoIgualdade.UseVisualStyleBackColor = true;
+      this.botaoIgualdade.Click += new System.EventHandler(this.botaoIgualdade_Click);
+      // 
+      // botaoC
+      // 
+      this.botaoC.Location = new System.Drawing.Point(174, 161);
+      this.botaoC.Name = "botaoC";
+      this.botaoC.Size = new System.Drawing.Size(75, 23);
+      this.botaoC.TabIndex = 18;
+      this.botaoC.Text = "C";
+      this.botaoC.UseVisualStyleBackColor = true;
+      this.botaoC.Click += new System.EventHandler(this.botaoC_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = global::Calculadora_Win.Properties.Resources.CalculadoraReduzida;
+      this.pictureBox1.Location = new System.Drawing.Point(333, 28);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(186, 185);
+      this.pictureBox1.TabIndex = 19;
+      this.pictureBox1.TabStop = false;
+      // 
+      // calculadoraForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(533, 231);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.botaoC);
+      this.Controls.Add(this.botaoIgualdade);
+      this.Controls.Add(this.botaoDivisao);
+      this.Controls.Add(this.botaoMultiplicacao);
+      this.Controls.Add(this.botaoSoma);
+      this.Controls.Add(this.botaoSubtracao);
+      this.Controls.Add(this.botao0);
+      this.Controls.Add(this.botao9);
+      this.Controls.Add(this.botao8);
+      this.Controls.Add(this.botao7);
+      this.Controls.Add(this.botao6);
+      this.Controls.Add(this.botao5);
+      this.Controls.Add(this.botao4);
+      this.Controls.Add(this.botao3);
+      this.Controls.Add(this.botao2);
+      this.Controls.Add(this.botao1);
+      this.Controls.Add(this.telaExibicao);
+      this.Name = "calculadoraForm";
+      this.Text = "Calculadora - Win";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox telaExibicao;
+    private System.Windows.Forms.Button botao1;
+    private System.Windows.Forms.Button botao2;
+    private System.Windows.Forms.Button botao3;
+    private System.Windows.Forms.Button botao4;
+    private System.Windows.Forms.Button botao5;
+    private System.Windows.Forms.Button botao6;
+    private System.Windows.Forms.Button botao7;
+    private System.Windows.Forms.Button botao8;
+    private System.Windows.Forms.Button botao9;
+    private System.Windows.Forms.Button botao0;
+    private System.Windows.Forms.Button botaoSubtracao;
+    private System.Windows.Forms.Button botaoSoma;
+    private System.Windows.Forms.Button botaoMultiplicacao;
+    private System.Windows.Forms.Button botaoDivisao;
+    private System.Windows.Forms.Button botaoIgualdade;
+    private System.Windows.Forms.Button botaoC;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
---
+++ calculadoraForm.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Win
+{
+  public partial class calculadoraForm : Form
+  {
+    private int resultado = 0;
+    public calculadoraForm()
+    {
+      InitializeComponent();
+
+      Bitmap bitMapCalc = new Bitmap(Properties.Resources.Calculadora);
+      this.Icon = Icon.FromHandle(bitMapCalc.GetHicon());
+
+      this.FormBorderStyle = FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+
+      this.telaExibicao.ReadOnly = true;
+    }
+
+    #region Eventos Click
+    private void botaoC_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Clear();
+    }
+
+    private void botao2_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "2";
+    }
+
+    private void botao3_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "3";
+    }
+
+    private void botaoSubtracao_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "-";
+    }
+
+    private void botao4_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "4";
+    }
+
+    private void botao5_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "5";
+    }
+
+    private void botao6_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "6";
+    }
+
+    private void botaoSoma_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "+";
+    }
+
+    private void botao7_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "7";
+    }
+
+    private void botao8_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "8";
+    }
+
+    private void botao9_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "9";
+    }
+
+    private void botaoMultiplicacao_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "*";
+    }
+
+    private void botao0_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "0";
+    }
+
+    private void botao1_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "1";
+    }
+
+    private void botaoDivisao_Click(object sender, EventArgs e)
+    {
+      this.telaExibicao.Text += "/";
+    }
+
+    private void botaoIgualdade_Click(object sender, EventArgs e)
+    {
+      string[] formulas = { "-", "+", "/", "*" };
+
+      if (!this.telaExibicao.Text.Contains("="))
+      {
+        foreach (string formula in formulas)
+        {
+          if (this.telaExibicao.Text.Contains(formula))
+          {
+            switch (formula)
+            {
+              case "+":
+                this.Soma(this.telaExibicao.Text);
+                break;
+
+              case "-":
+                this.Subtracao(this.telaExibicao.Text);
+                break;
+
+              case "*":
+                this.Multiplicacao(this.telaExibicao.Text);
+                break;
+
+              case "/":
+                this.Divisao(this.telaExibicao.Text);
+                break;
+
+              default:
+                this.telaExibicao.Clear();
+                break;
+            }
+          }
+        }
+
+        this.telaExibicao.Text += string.Format(" = " + resultado);
+        this.resultado = 0;
+      }
+      else
+      {
+        this.telaExibicao.Clear();
+      }
+
+    }
+
+    #endregion
+
+    #region Formulas
+    private int Soma(string expressao)
+    {
+      string[] valores = expressao.Split('+');
+      int indice = 0;
+
+      foreach (string valor in valores)
+      {
+        if (indice == 0)
+        {
+          this.resultado = Convert.ToInt32(valor);
+          indice++;
+        }
+        else
+          this.resultado += Convert.ToInt32(valor);
+      }
+      return resultado;
+    }
+
+    private int Subtracao(string expressao)
+    {
+      string[] valores = expressao.Split('-');
+      int indice = 0;
+
+      foreach (string valor in valores)
+      {
+        if (indice == 0)
+        {
+          this.resultado = Convert.ToInt32(valor);
+          indice++;
+        }
+        else
+          this.resultado -= Convert.ToInt32(valor);
+      }
+      return resultado;
+    }
+
+    private int Divisao(string expressao)
+    {
+      string[] valores = expressao.Split('/');
+      int indice = 0;
+
+      foreach (string valor in valores)
+      {
+        if (indice == 0)
+        {
+          this.resultado = Convert.ToInt32(valor);
+          indice++;
+        }
+        else
+          this.resultado /= Convert.ToInt32(valor);
+      }
+      return resultado;
+    }
+
+    private int Multiplicacao(string expressao)
+    {
+      string[] valores = expressao.Split('*');
+      int indice = 0;
+
+      foreach (string valor in valores)
+      {
+        if (indice == 0)
+        {
+          this.resultado = Convert.ToInt32(valor);
+          indice++;
+        }
+        else
+          this.resultado *= Convert.ToInt32(valor);
+      }
+      return resultado;
+    }
+  #endregion
+  }
+}
